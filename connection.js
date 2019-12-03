1140c27d740f4b5d537c3f03b0eb6a28742ae86f
Michael24566 <56282412+Michael24566@users.noreply.github.com>
2019-12-02 20:32:26 -0600

    Added the rest of our current stored procedures
    all 9 stored procedures now have related functions

--- connection.js
+++ connection.js
@@ -66,6 +66,96 @@ app.get('/getCrimesByBlock', async function (req, res){
     }
 });
 
+app.get('/getCrimesByBlockWithTime', async function (req, res){
+    await dbConnect;
+    try {
+        var request = db.request();
+        var result = await request
+            .input('Block', sql.VarChar(255), "089XX S COTTAGE GROVE AVE")
+            .input('dateMin', sql.VarChar(255), '2005')
+            .input('dateMax', sql.VarChar(255), '2005')
+            .execute('getCrimesByBlockWithTime');
+        console.dir(result);
+        res.json(result);
+    } catch (err) {
+        console.error('SQL error', err);
+    }
+});
+
+app.get('/getCrimesByDistrict', async function (req, res){
+    await dbConnect;
+    try {
+        var request = db.request();
+        var result = await request
+            .input('District', sql.VarChar(255), "25")
+            .execute('getCrimesByDistrict');
+        console.dir(result);
+        res.json(result);
+    } catch (err) {
+        console.error('SQL error', err);
+    }
+});
+
+app.get('/getCrimesByDistrictWithTime', async function (req, res){
+    await dbConnect;
+    try {
+        var request = db.request();
+        var result = await request
+            .input('District', sql.VarChar(255), "25")
+            .input('dateMin', sql.VarChar(255), '2005')
+            .input('dateMax', sql.VarChar(255), '2005')
+            .execute('getCrimesByDistrictWithTime');
+        console.dir(result);
+        res.json(result);
+    } catch (err) {
+        console.error('SQL error', err);
+    }
+});
+
+app.get('/getCrimesByType', async function (req, res){
+    await dbConnect;
+    try {
+        var request = db.request();
+        var result = await request
+            .input('primaryType', sql.VarChar(255), "Arson")
+            .execute('getCrimesByType');
+        console.dir(result);
+        res.json(result);
+    }catch(err){
+        console.error('SQL error', err);
+    }
+});
+
+app.get('/getCrimesByWard', async function (req, res){
+    await dbConnect;
+    try {
+        var request = db.request();
+        var result = await request
+            .input('Ward', sql.VarChar(255), "7")
+            .execute('getCrimesByWard');
+        console.dir(result);
+        res.json(result);
+    } catch (err) {
+        console.error('SQL error', err);
+    }
+});
+
+app.get('/getCrimesByWardWithTime', async function (req, res){
+    await dbConnect;
+    try {
+        var request = db.request();
+        var result = await request
+            .input('Ward', sql.VarChar(255), "7")
+            .input('dateMin', sql.VarChar(255), '2005')
+            .input('dateMax', sql.VarChar(255), '2005')
+            .execute('getCrimesByWardWithTime');
+        console.dir(result);
+        res.json(result);
+    } catch (err) {
+        console.error('SQL error', err);
+    }
+});
+
 app.listen(8000, () => {
     console.log("listening on port 8000");
 });
