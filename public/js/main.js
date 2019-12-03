9430fd7312016cb3cb0c56212fc5e9a68a8fc5f8
StrickerGitsIt <56243053+StrickerGitsIt@users.noreply.github.com>
2019-12-02 21:21:07 -0600

    Merge branch 'Dev' of https://github.com/BryanFratianne/Group9 into Dev
    # Conflicts:
    #	.idea/workspace.xml
    #	connection.js

--- public/js/main.js
+++ public/js/main.js
@@ -5,20 +5,50 @@ $(document).ready(async function(){
     //console.log(iucrList);
 
 
-    var primarySearch = "crimeType";
-
+    var crimeType = "crimeType";
+    var timeStart = "2000";
+    var timeEnd = "2020";
+    var block = "NULL";
+    var district = "NULL";
+    var ward = "NULL";
 
     $("#toggleButton").click(function(){
-        alert("hello");
+        alert("shouldToggleHere");
     });
 
 
     $("select#CrimeType").change(function() {
-        primarySearch = $("#searchTypeSelection").val();
-        alert("hi");
+        primarySearch = this.value;
+        alert("crimeType changed");
     });
 
+    $("#yearStart").change(function(){
+        var temp = parseInt(this.value);
+        if(temp > timeEnd){
+            alert("Year range invalid")
+            $(this).val(timeStart);
+        }
+        else {
+            timeStart = this.value;
+            alert("changed start year");
+        }
+    });
+    $("#yearEnd").change(function(){
+        var temp = parseInt(this.value);
+        if(temp < timeStart){
+            alert("Year range invalid")
+            $(this).val(timeEnd);
+        }
+        else {
+            timeEnd = this.value;
+            alert("changed end year");
+        }
+    });
 
+    $("#submit").click(function(){
+        //You can put in the queuryr here, selects queuery on the data selected. 
+
+    })
 
 
 
